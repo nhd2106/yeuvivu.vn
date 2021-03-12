c875b6a1015ddf6f47051b2ce7b30f763798320d
Duoc Ngo <nhd.ars@gmail.com>
2021-03-12 19:18:07 +0700

    image seo

--- html/node/next-seo.config.js
+++ html/node/next-seo.config.js
@@ -7,6 +7,14 @@ export default {
         locale: 'vi_VN',
         url : 'http://yeuvivu.vn/',
         site_name: 'yeuvivu',
+        images : [
+            {
+                url: 'https://yeuvivu.vn:1337/uploads/vagabond_cafe_ha_noi_1_62962b07e9.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Og Image Alt',
+            }
+        ]
         
     }
 }
