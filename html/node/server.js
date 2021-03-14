ae8ac2fa3b0d9bbf572e6fdb3ac30f55ca166a53
duoc ngo <nhd.ars@gmail.com>
2021-03-14 03:50:57 +0000

    nothing

--- html/node/server.js
+++ html/node/server.js
@@ -8,16 +8,17 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-  key: fs.readFileSync('./certificates/privkey.pem'),
-  cert: fs.readFileSync('./certificates/fullchain.pem')
+  key: fs.readFileSync('../../../../../../etc/letsencrypt/live/yeuvivu.vn/privkey.pem'),
+  cert: fs.readFileSync('../../../../../../etc/letsencrypt/live/yeuvivu.vn/fullchain.pem')
 };
 
 app.prepare().then(() => {
+  console.log(httpsOptions.key)
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
     
-  }).listen(443, err => {
+  }).listen(3000, err => {
     if (err) throw err;
     console.log('> Ready on https://localhost:3000');
   });
